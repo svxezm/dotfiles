fadad39182b8b3309a9f01bf94855c4bd45f6762
monitz <124447707+monitzz@users.noreply.github.com>
2024-06-23 18:33:41 -0300

    Add files via upload

---
+++ hypr/startup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Start Waybar
+waybar &
+
+firefox &
+
+# Start Tofi (for example, as an application launcher)
+#tofi-drun --drun-launch=true &
+
+# Start background service (adjust to your background setting tool)
+swaybg -i /path/to/your/wallpaper.jpg &
+
+# Other utilities
+# nm-applet &       # NetworkManager applet
+# blueman-applet &  # Bluetooth manager

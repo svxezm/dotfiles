be491d215ff1d312e7cc7cc3f7ba1105cae9f78c
svxezm <igorb.kuhl@gmail.com>
2024-11-25 15:41:19 -0300

    Set cursor theme to rose-pine on system startup

--- hypr/startup.sh
+++ hypr/startup.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+export XCURSOR_THEME="BreezeX-RosePine-Linux"
+export XCURSOR_SIZE="24"
+
+xrdb -merge <<< "Xcursor.theme: $XCURSOR_THEME"
+xrdb -merge <<< "Xcursor.size: $XCURSOR_SIZE"
+
 # Start Waybar
 waybar &
 
 firefox &
+
+tmux &
 
 # Start Tofi (for example, as an application launcher)
 # tofi-drun --drun-launch=true &

c9ed8ea3150ecc075100f2591b00958aa6e3386c
svxezm <igorb.kuhl@gmail.com>
2025-01-21 23:35:14 -0300

    feat: set run commands for cpp and rust

--- config/nvim/after/plugin/code_runner.lua
+++ config/nvim/after/plugin/code_runner.lua
@@ -1,29 +1,30 @@
 require('code_runner').setup({
-
     filetype = {
         python = "python3 -u",
         typescript = "bun run",
         javascript = "bun run",
         cs = "dotnet run",
-        c = function(...)
-            c_base = {
-                "cd $dir &&",
-                "gcc $fileName -o",
-                "/tmp/$fileNameWIthoutExt",
-            }
-            local c_exec = {
-                "&& /tmp/$fileNameWithoutExt &&",
-                "rm /tmp/$fileNameWithoutExt",
-            }
-            vim.ui.input({ prompt = "Add more args:" }, function(input)
-                c_base[4] = input
-                vim.print(vim.tbl_extend("force", c_base, c_exec))
-                require("code_runner.commands").run_from_fn(vim.list_extend(c_base,c_exec))
-            end)
-        end,
+        c = {
+            "cd $dir &&",
+            "gcc $fileName -o $fileNameWithoutExt &&",
+            "$dir/$fileNameWithoutExt",
+        },
+        cpp = {
+            "cd $dir &&",
+            "/usr/bin/g++ -std=c++23 -o $dir/$fileNameWithoutExt $fileName &&",
+            "$dir/$fileNameWithoutExt",
+        },
+        sh = "bash",
+        ruby = "ruby",
+        rust = {
+            "cd $dir &&",
+            "cargo $fileName &&",
+            "$dir/target/debug/$fileNameWithoutExt",
+        },
     },
 })
 
+-- Add your key mappings
 vim.keymap.set('n', '<leader>r', ':RunCode<CR>', { noremap = true, silent = false })
 vim.keymap.set('n', '<leader>rf', ':RunFile<CR>', { noremap = true, silent = false })
 vim.keymap.set('n', '<leader>rft', ':RunFile tab<CR>', { noremap = true, silent = false })

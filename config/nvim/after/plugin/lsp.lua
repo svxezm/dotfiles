9e9bec53fa473311313840563b25e59f153d3d70
svxezm <igorb.kuhl@gmail.com>
2025-01-21 23:35:58 -0300

    feat: ensure rust_analyzer and ast_grep to be installed

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,5 +1,19 @@
 local lsp = require('lsp-zero')
 
+lsp.on_attach(function(client,bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {'rust_analyzer', 'ast_grep'},
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  }
+})
+
 lsp.preset('recommended')
 
 local cmp = require('cmp')
